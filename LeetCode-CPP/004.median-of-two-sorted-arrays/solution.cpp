29e610639151b52029f0bcb43356c74278d3d1b2
smart <88cxjwin@gmail.com>
2020-06-02 01:18:21 +0800

    complete 004

--- LeetCode-CPP/004.median-of-two-sorted-arrays/solution.cpp
+++ LeetCode-CPP/004.median-of-two-sorted-arrays/solution.cpp
@@ -40,9 +40,43 @@ public:
         }
     }
 
+    int getKthElement(vector<int>& nums1, vector<int>& nums2, int k) {
+        int m = nums1.size(), n = nums2.size();
+        int index1 = 0, index2 = 0;
+        while (true) {
+            if (index1 == m) {
+                return nums2[index2 + k - 1];
+            }
+            if (index2 == n) {
+                return nums1[index1 + k  - 1];
+            }
+            if (k == 1) {
+                return min(nums1[index1], nums2[index2]);
+            }
+            int newIndex1 = min(index1 + k / 2 - 1, m - 1);
+            int newIndex2 = min(index2 + k / 2 - 1, n - 1);
+            int pivot1 = nums1[newIndex1];
+            int pivot2 = nums2[newIndex2];
+            if (pivot1 <= pivot2) {
+                k -= newIndex1 + 1 - index1;
+                index1 = newIndex1 + 1;
+            } else {
+                k -= newIndex2 + 1 - index2;
+                index2 = newIndex2 + 1;
+            }
+        }
+    }
+
     double findMedianSortedArrays2(vector<int>& nums1, vector<int>& nums2) {
-        // TODO:
-        return 0;
+        int m = nums1.size(), n = nums2.size();
+        int total = m + n;
+        if ((m + n) % 2 == 0) {
+            int median1 = getKthElement(nums1, nums2, total / 2);
+            int median2 = getKthElement(nums1, nums2, total / 2 + 1);
+            return (median1 + median2) / 2.0;
+        } else {
+            return getKthElement(nums1, nums2, (total + 1) / 2);
+        }
     }
 
     double findMedianSortedArrays3(vector<int>& nums1, vector<int>& nums2) {
@@ -90,6 +124,24 @@ TEST_CASE( "004-1", "[findMedianSortedArrays]" ) {
         vector<int> nums1 = {1, 2};
         vector<int> nums2 = {3, 4};
         REQUIRE( sl.findMedianSortedArrays(nums1, nums2) == 2.5 );
+    }
+}
+
+TEST_CASE( "004-2", "[findMedianSortedArrays2]" ) {
+    Solution sl = Solution();
+
+    // case1
+    {
+        vector<int> nums1 = {1, 3};
+        vector<int> nums2 = {2};
+        REQUIRE( sl.findMedianSortedArrays2(nums1, nums2) == 2 );
+    }
+
+    // case2
+    {
+        vector<int> nums1 = {1, 2};
+        vector<int> nums2 = {3, 4};
+        REQUIRE( sl.findMedianSortedArrays2(nums1, nums2) == 2.5 );
     }
 }
 
